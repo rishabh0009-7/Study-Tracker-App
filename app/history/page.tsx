c94c8a58fe320fad1d8cc1a596dab030c0491472
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-09-15 20:31:45 +0530

    fixed deployment error

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -117,7 +117,7 @@ export default async function HistoryPage() {
         <div
           className="absolute inset-0 opacity-10"
           style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.05'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width=&apos;60&apos; height=&apos;60&apos; viewBox=&apos;0 0 60 60&apos; xmlns=&apos;http://www.w3.org/2000/svg&apos;%3E%3Cg fill=&apos;none&apos; fill-rule=&apos;evenodd&apos;%3E%3Cg fill=&apos;%23ffffff&apos; fill-opacity=&apos;0.05&apos;%3E%3Ccircle cx=&apos;30&apos; cy=&apos;30&apos; r=&apos;2&apos;/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
           }}
         ></div>
         {/* Subtle floating elements */}
